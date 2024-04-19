589aac61f4251f873a41c6cf1b28ca0320890715
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 00:20:48 +0500

    Добавил persistor для pinia

--- main.ts
+++ main.ts
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import { AppEntry } from './src/app'
 import { router } from './src/app/router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(AppEntry)
 
 app.use(router)
