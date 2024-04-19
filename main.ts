5f9f2b2b0f8c48ce037ed2d8835e9699b9313110
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 00:10:59 +0500

    Подключил pinia

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import { AppEntry } from './src/app'
 import { router } from './src/app/router'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(AppEntry)
 
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
