cb6e59c395d616057386f0ad7d09f649842c7e2e
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 14:44:37 +0500

    Добавил checkbox

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 import Logo from './Logo/index.vue'
 import Counter from './Counter/index.vue'
 import Input from './Form/Input/index.vue'
+import Checkbox from './Form/Checkbox/index.vue'
 
-export { Logo, Counter, Input }
+export { Logo, Counter, Input, Checkbox }
