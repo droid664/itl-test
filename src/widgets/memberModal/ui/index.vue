03b79bfa8798feff408ca1fa54314f069325f074
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 21:18:38 +0500

    Добавил поля в модалку

--- src/widgets/memberModal/ui/index.vue
+++ src/widgets/memberModal/ui/index.vue
@@ -1,12 +1,106 @@
 <template>
   <Teleport to="body">
-    <Modal :isShow="isShow"></Modal>
+    <Modal :isShow="modalStore.isShow">
+      <form class="form">
+        <div
+          v-for="field of Object.values(fields)"
+          :key="field.name"
+          class="form__item"
+          :class="field.className"
+        >
+          <p class="form__item-name">{{ field.name }}</p>
+          <template v-if="field.type === 'text'">
+            <Input v-model="field.value" />
+          </template>
+          <template v-if="field.type === 'select' && field.options">
+            <Select :options="field.options" />
+          </template>
+          <template v-if="field.type === 'checkbox'">
+            <Checkbox v-model="field.value" />
+          </template>
+        </div>
+      </form>
+    </Modal>
   </Teleport>
 </template>
 
 <script lang="ts" setup>
 import { MemberModal } from '../model'
 import { Modal } from '../../../shared'
+import { ref } from 'vue'
+import { Input } from '../../../shared'
+import { Select } from '../../../shared'
+import { Checkbox } from '../../../shared'
 
-const { isShow } = MemberModal()
+const modalStore = MemberModal()
+
+modalStore.$patch({
+  isShow: true,
+})
+
+const fields = ref([
+  {
+    name: 'ФИО',
+    type: 'text',
+    required: true,
+    value: '',
+    className: 'form__item-name--fio',
+  },
+  {
+    name: 'Компания',
+    type: 'text',
+    required: true,
+    value: '',
+    className: 'form__item-name--company',
+  },
+  {
+    name: 'Группа',
+    type: 'select',
+    options: ['Прохожий', 'Клиент', 'Партнер'],
+    required: true,
+    value: '',
+    className: 'form__item-name--group',
+  },
+  {
+    name: 'Присутсвие',
+    type: 'checkbox',
+    required: true,
+    value: false,
+    className: 'form__item-name--presence',
+  },
+])
+
+console.log(fields)
 </script>
+
+<style lang="scss">
+.form {
+  &__item {
+    display: flex;
+    align-items: center;
+    &-name {
+      margin-bottom: 0;
+      min-width: 275px;
+      padding-right: 20px;
+      font-size: 32px;
+      font-weight: 600;
+      &--fio {
+        margin-bottom: 40px;
+      }
+      &--company {
+        margin-bottom: 57px;
+      }
+      &--group {
+        margin-bottom: 75px;
+      }
+      &--presence {
+        margin-bottom: 48px;
+      }
+    }
+    .input,
+    .select__value {
+      width: 502px;
+    }
+  }
+}
+</style>
