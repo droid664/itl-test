8f646dd4fe0ea10f006291e20aac23453f469411
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 20:33:30 +0500

    Добавил modal

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,5 +3,6 @@ import Counter from './Counter/index.vue'
 import Input from './Form/Input/index.vue'
 import Checkbox from './Form/Checkbox/index.vue'
 import Select from './Form/Select/index.vue'
+import Modal from './Modal/index.vue'
 
-export { Logo, Counter, Input, Checkbox, Select }
+export { Logo, Counter, Input, Checkbox, Select, Modal }
