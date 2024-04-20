093b0d008c71aad8566d37e704552742819667ff
Droid664 <iskandarov.murat@gmail.com>
2024-04-20 15:30:42 +0500

    Добавил элемент select

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,5 +2,6 @@ import Logo from './Logo/index.vue'
 import Counter from './Counter/index.vue'
 import Input from './Form/Input/index.vue'
 import Checkbox from './Form/Checkbox/index.vue'
+import Select from './Form/Select/index.vue'
 
-export { Logo, Counter, Input, Checkbox }
+export { Logo, Counter, Input, Checkbox, Select }
