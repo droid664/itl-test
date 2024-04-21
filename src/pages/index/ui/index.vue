f27e43c71e2a6ea17ef71bdc55c54a4a00daeba5
Droid664 <iskandarov.murat@gmail.com>
2024-04-21 13:09:22 +0500

    Правки по вёрстке

--- src/pages/index/ui/index.vue
+++ src/pages/index/ui/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="index-page">
-    <MemberHeader>
-      <template #count>
-        <MemberVisitors />
-      </template>
-    </MemberHeader>
+    <div class="index-page__top">
+      <MemberHeader>
+        <template #count>
+          <MemberVisitors />
+        </template>
+      </MemberHeader>
+    </div>
     <MemberListWidget />
-    <MemberFilter />
     <MemberModal />
+    <div class="index-page__bottom">
+      <MemberFilter />
+    </div>
   </div>
 </template>
 
@@ -17,3 +21,22 @@ import { MemberFilter } from '../../../features/MemberFilter'
 import { MemberModal } from '../../../widgets'
 import { MemberHeader } from '../../../widgets/memberHeader'
 </script>
+
+<style lang="scss" scoped>
+.index-page {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+  padding-top: 24px;
+  &__top {
+    margin-bottom: 34px;
+  }
+  &__bottom {
+    position: sticky;
+    bottom: 0;
+    background: var(--white);
+    padding-top: 46px;
+    padding-bottom: 46px;
+  }
+}
+</style>
